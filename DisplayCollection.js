f6da39c1cfeb62d6d69b042d5163f722cb6c9c42
DominikColl <DAColl@student.fullsail.edu>
2019-06-25 18:18:29 -0400

    Links are now used for the username being displayed

--- DisplayCollection.js
+++ DisplayCollection.js
@@ -3,7 +3,7 @@ class Display{
         //collection is array of users
         let html=``;
         collection.forEach(user => {
-        html+=`<li><span>${user.name}</span> ${user.viewCount}<div>${user.live}</div></li>`;
+        html+=`<li><span><a href="${Logic.createURL(user.name)}">${user.name}</a></span> ${user.viewCount}<div>${user.live}</div></li>`;
             
         });
         document.querySelector('ul').innerHTML=html;
--- Logic.js
+++ Logic.js
@@ -4,4 +4,9 @@ class Logic{
         e.style.backgroundColor='#85bb65';
       }
     }
+    //links working
+    //creates link to users live stream
+    static createURL(username){
+      return `https://www.twitch.tv/${username}`;
+    }
   }
